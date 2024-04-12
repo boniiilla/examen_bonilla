70b59b629c995d461e537e2945d62e7099462dc9
boniiilla <c.bonillac00@gmail.com>
2024-04-12 09:51:02 +0200

    Prueba del paquete

--- sorting/main.py
+++ sorting/main.py
@@ -1,1 +1,6 @@
 from sorting import Sorting
+
+class Main:
+    ordenar = Sorting()
+    ordenar.ordenar_numeros()
+app = Main()
--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,10 +1,24 @@
 '''
-1. Ordenar una lista de nombres
-2.1. pedir nombres
-2.2. guardarlos en una lista
+1. Ordenar una lista de numeros
+2.1. pedir numeros y guardarlos en una lista
 2.3. ordernarlos
 '''
 
 class Sorting:
     def __init__(self):
-        self.nombres = []
+        self.numeros = []
+    
+    def pedir_numeros(self):
+        self.numero = 0
+        print("Introduce 10 numeros: ")
+        for i in range(10):
+            self.numero = int(input())
+            self.numeros.append(self.numero)
+
+    def ordenar_numeros(self):
+        Sorting.pedir_numeros(self)
+        error = False
+        for numero in range(len(self.numeros)):
+            if self.numeros[numero] > self.numeros[numero +1] and error == False:
+                self.numeros[numero], self.numeros[numero +1] = self.numeros[numero +1], self.numeros[numero]
+                print(numero)
